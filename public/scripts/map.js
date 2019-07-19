7a85281221391b3bb2c939e9966095cd0f16cc27
Brian K <>
2019-07-18 22:11:58 -0400

    Added layers and zoom to location

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,23 +1,77 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg';
+L.mapbox.accessToken = 'pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg';
 
-var map = new mapboxgl.Map({
-container: 'map', // container id
-style: 'mapbox://styles/ketchem2/cjy7rq3i508iy1cmpqt7uevfj', // stylesheet location
-center: [-82.351, 36.273681], // starting position [lng, lat]
-zoom: 13.5 // starting zoom
-});
+var map = L.mapbox.map('map').setView([36.2740527, -82.34555], 15);
 
-map.on('load', function(){
-    addTrails();
-});
+L.mapbox.styleLayer('mapbox://styles/ketchem2/cjy7rq3i508iy1cmpqt7uevfj').addTo(map);
+
+var trailStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+addTrails();
+addParkingLoop();
+addRoad();
+
 
 function addTrails(){
-    $.ajax("/data/BuffaloMountainTrials.json", {
+    $.ajax("data/BuffaloMtnTrails.geojson", {
         dataType: "json",
         success: createTrailsLayer
     });
 };
 
 function createTrailsLayer(response, status, jqXHRobject){
-    console.log(response);
+    var trailsLayer = L.geoJSON(response, trailStyle).addTo(map);
+
+    trailsLayer.bringToFront(map);
+    console.log("here");
 };
+
+function addParkingLoop(){
+    $.ajax("data/BuffaloMtnParking.geojson", {
+        dataType: "json",
+        success: createParkingLoopLayer
+    });
+};
+
+function createParkingLoopLayer(response, status, jqXHRobject){
+    var trailsLayer = L.geoJSON(response, trailStyle).addTo(map);
+
+    trailsLayer.bringToFront(map);
+    console.log("here");
+};
+
+function addRoad(){
+    $.ajax("data/BuffaloMtnRoad.geojson", {
+        dataType: "json",
+        success: createRoadLayer
+    });
+};
+
+function createRoadLayer(response, status, jqXHRobject){
+    var trailsLayer = L.geoJSON(response, trailStyle).addTo(map);
+
+    trailsLayer.bringToFront(map);
+    console.log("here");
+};
+
+map.on('click', function(e){
+    var coord = e.latlng;
+    var lat = coord.lat;
+    var lng = coord.lng;
+    console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+});
+
+map.locate({setView: true, maxZoom: 16});
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
