c8a89e6489433261cbcadc164131b6039d106f91
Brian K <>
2019-07-19 21:32:17 -0400

    Added Location Toggle

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -4,6 +4,50 @@ var map = L.mapbox.map('map').setView([36.2740527, -82.34555], 15);
 
 L.mapbox.styleLayer('mapbox://styles/ketchem2/cjy7rq3i508iy1cmpqt7uevfj').addTo(map);
 
+var locationButton;
+var locationOn = false;
+var locationMarker;
+var locationRadius;
+
+var findMyLocation = L.Control.extend({
+ 
+    options: {
+        position: 'topleft' 
+    },
+ 
+    onAdd: function (map) {
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom location-button');
+        container.innerHTML = '<i class="fas fa-location"></i>';
+        container.onclick = toggleLocation;
+        return container;
+    }
+});
+
+
+
+// locationButton.innerHTML = 'A'
+
+map.addControl(new findMyLocation());
+
+locationButton = $(".location-button")[0];
+
+console.log(locationButton);
+
+
+function toggleLocation(){
+    if (locationOn){
+        removeLocation();
+        locationOn = false;
+    }
+    else {
+        map.locate({setView: true, maxZoom: 16});
+        map.on('locationfound', onLocationFound);
+        locationOn = true;
+    }
+}
+
+
+
 var trailStyle = {
     "color": "#ff7800",
     "weight": 5,
@@ -26,7 +70,7 @@ function createTrailsLayer(response, status, jqXHRobject){
     var trailsLayer = L.geoJSON(response, trailStyle).addTo(map);
 
     trailsLayer.bringToFront(map);
-    console.log("here");
+    // console.log("here");
 };
 
 function addParkingLoop(){
@@ -40,7 +84,7 @@ function createParkingLoopLayer(response, status, jqXHRobject){
     var trailsLayer = L.geoJSON(response, trailStyle).addTo(map);
 
     trailsLayer.bringToFront(map);
-    console.log("here");
+    // console.log("here");
 };
 
 function addRoad(){
@@ -53,25 +97,31 @@ function addRoad(){
 function createRoadLayer(response, status, jqXHRobject){
     var trailsLayer = L.geoJSON(response, trailStyle).addTo(map);
 
-    trailsLayer.bringToFront(map);
-    console.log("here");
+    // trailsLayer.bringToFront(map);
+    // console.log("here");
 };
 
-map.on('click', function(e){
-    var coord = e.latlng;
-    var lat = coord.lat;
-    var lng = coord.lng;
-    console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
-});
+// map.on('click', function(e){
+//     var coord = e.latlng;
+//     var lat = coord.lat;
+//     var lng = coord.lng;
+//     console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+// });
 
-map.locate({setView: true, maxZoom: 16});
+
 function onLocationFound(e) {
     var radius = e.accuracy;
 
-    L.marker(e.latlng).addTo(map)
+    locationMarker = L.marker(e.latlng, {customId: "yourLocation"}).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-    L.circle(e.latlng, radius).addTo(map);
+    locationRadius = L.circle(e.latlng, radius, {customId :"locationRadius"}).addTo(map);
 }
 
-map.on('locationfound', onLocationFound);
+
+function removeLocation(){
+    console.log('remove layer method hit');
+    locationMarker.remove(map);
+    locationRadius.remove(map);
+};
+
