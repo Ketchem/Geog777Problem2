69a113fb73e4d8a50e60352c322f36012b4455b3
Brian K <>
2019-07-22 17:21:30 -0400

    installed pg, connected to postgres db

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+var express = require('express'),
+    bodyParser = require('body-parser'), 
+    app = express();
+
+
+const { Pool, Client } = require('pg')
+const connectionString = 'postgresql://postgres:admin@192.168.86.120:5432/ParkApp'
+    
+const pool = new Pool({
+      connectionString: connectionString,
+    });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
@@ -11,7 +19,15 @@ app.get("/", function(req, res){
 });
 
 app.get("/map", function(req, res){
-    res.render("map");
+    pool.query('SELECT trailid, name, description, length, difficulty, type, parkid, ST_AsGeoJSON(geom) as geometry FROM trail', (err, trails) => {
+        if (err) {
+          throw err
+        }
+        else {
+            // console.log(trails.rows)
+            res.render("map", {trails:trails.rows});
+        }
+    });
 });
 
 app.listen(3000, "localhost", function(){
